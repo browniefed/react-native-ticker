d37e3e1d7a7199a515839acab3079cfed3621201
Jason Brown <browniefed@gmail.com>
2018-01-23 09:40:28 -0800

    Add in new features for custom tickers

--- README.md
+++ README.md
@@ -10,20 +10,82 @@ npm install react-native-ticker
 ```
 
 ```js
-import RotateText from "react-native-ticker";
+import Ticker from "react-native-ticker";
 
 // As a text prop
-<RotateText text={"1235.44"} textStyle={styles.text} rotateTime={250} />;
+<Ticker text={"1235.44"} textStyle={styles.text} rotateTime={250} />;
 
 // Or as a child
-<RotateText textStyle={styles.text} rotateTime={250}>
+<Ticker textStyle={styles.text} rotateTime={250}>
   12345.44
-</RotateText>
+</Ticker>
 
 ```
 
-Supply a `textStyle`, as well as `text`.
+Supply a `textStyle`, as well as `text`. `rotateTime` is optional and defaults to `250ms`.
 
-`rotateTime` is optional and defaults to `250ms`.
+
+If you need more than just numbers you can build and supply your own rotations.
+
+```js
+import Ticker, { Tick } from "react-native-ticker";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#333",
+  },
+  text: {
+    fontSize: 40,
+    color: "#FFF",
+  },
+});
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const currencies = ["$", "¥", "€"];
+
+export default class App extends Component {
+  state = {
+    currency: "$",
+    value: "123",
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        currency: currencies[getRandom(0, 2)],
+        value: getRandom(0, 1000) + "",
+      });
+    }, 500);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ticker textStyle={styles.text}>
+          <Tick rotateItems={currencies}>{this.state.currency}</Tick>
+          {this.state.value.split("").map((char, i) => {
+            return (
+              <Tick key={i} rotateItems={numbers}>
+                {char}
+              </Tick>
+            );
+          })}
+          !!!
+        </Ticker>
+      </View>
+    );
+  }
+}
+```
+
+You must render a `Ticker` and subsequently at least one `Tick`. If it is text then it will simply be rendered.
 
 ![](./examples.gif)
--- index.js
+++ index.js
@@ -16,10 +16,14 @@ const styles = StyleSheet.create({
 });
 
 const range = length => Array.from({ length }, (x, i) => i);
-const getPosition = (number, height) => parseFloat(number, 10) * -1 * height;
+const getPosition = ({ text, items, height }) => {
+  const index = items.findIndex(p => p === text);
+  return index * height * -1;
+};
 const splitText = (text = "") => (text + "").split("");
 const isNumber = (text = "") => !isNaN(parseInt(text, 10));
-const measureText = range(10);
+const isString = (text = "") => typeof text === "string";
+const measureText = range(10).map(p => p + "");
 
 const getAnimationStyle = animation => {
   return {
@@ -39,7 +43,7 @@ const Piece = ({ children, style, textStyle }) => {
   );
 };
 
-class RotateText extends Component {
+class Ticker extends Component {
   static propTypes = {
     text: PropTypes.string,
     textStyle: PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.array]),
@@ -70,24 +74,18 @@ class RotateText extends Component {
     const { height, measured } = this.state;
     const opacity = measured ? 1 : 0;
 
+    const childs = text || children;
+
+    const renderer = isString(childs) ? stringNumberRenderer : generalChildRenderer;
+
     return (
       <View style={[styles.row, { height, opacity }, style]}>
-        {splitText(text || children).map((piece, i) => {
-          if (!isNumber(piece))
-            return (
-              <Piece key={i} style={{ height }} textStyle={textStyle}>
-                {piece}
-              </Piece>
-            );
-          return (
-            <TextRotator
-              duration={rotateTime}
-              key={i}
-              text={piece}
-              textStyle={textStyle}
-              height={height}
-            />
-          );
+        {renderer({
+          children: childs,
+          textStyle,
+          height,
+          rotateTime,
+          rotateItems: measureText,
         })}
         <Text style={[textStyle, styles.hide]} onLayout={this.handleMeasure} pointerEvents="none">
           0
@@ -97,35 +95,99 @@ class RotateText extends Component {
   }
 }
 
-// If we are first starting out then just render at 0 height
-// Else if we already know our height set to 0 so we can animate it into place
-const getHeight = height => (height === 0 ? height : 0);
+const generalChildRenderer = ({ children, textStyle, height, rotateTime, rotateItems = [] }) => {
+  return React.Children.map(children, (child, i) => {
+    debugger;
+    if (isString(child)) {
+      return (
+        <Piece style={{ height }} textStyle={textStyle}>
+          {child}
+        </Piece>
+      );
+    }
 
-class TextRotator extends Component {
+    const items = child.props.rotateItems || rotateItems;
+    const key = items.join(",") + i;
+
+    return React.cloneElement(child, {
+      key,
+      text: child.props.text || child.props.children,
+      height,
+      duration: child.props.rotateTime || rotateTime,
+      textStyle,
+      rotateItems: child.props.rotateItems || rotateItems,
+    });
+  });
+};
+
+const stringNumberRenderer = ({ children, textStyle, height, rotateTime, rotateItems }) => {
+  return splitText(children).map((piece, i) => {
+    if (!isNumber(piece))
+      return (
+        <Piece key={i} style={{ height }} textStyle={textStyle}>
+          {piece}
+        </Piece>
+      );
+    return (
+      <Tick
+        duration={rotateTime}
+        key={i}
+        text={piece}
+        textStyle={textStyle}
+        height={height}
+        rotateItems={rotateItems}
+      />
+    );
+  });
+};
+
+class Tick extends Component {
   state = {
-    animation: new Animated.Value(getPosition(this.props.text, getHeight(this.props.height))),
+    animation: new Animated.Value(
+      getPosition({
+        text: this.props.text,
+        items: this.props.rotateItems,
+        height: this.props.height,
+      }),
+    ),
   };
   componentDidMount() {
     // If we first render then don't do a mounting animation
     if (this.props.height !== 0) {
       this.setState({
-        animation: new Animated.Value(getPosition(this.props.text, this.props.height)),
+        animation: new Animated.Value(
+          getPosition({
+            text: this.props.text,
+            items: this.props.rotateItems,
+            height: this.props.height,
+          }),
+        ),
       });
     }
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.height !== this.props.height) {
       this.setState({
-        animation: new Animated.Value(getPosition(nextProps.text, nextProps.height)),
+        animation: new Animated.Value(
+          getPosition({
+            text: nextProps.text,
+            items: nextProps.rotateItems,
+            height: nextProps.height,
+          }),
+        ),
       });
     }
   }
   componentDidUpdate(prevProps) {
-    const { height, duration } = this.props;
+    const { height, duration, rotateItems, text } = this.props;
 
-    if (prevProps.text !== this.props.text) {
+    if (prevProps.text !== text) {
       Animated.timing(this.state.animation, {
-        toValue: getPosition(this.props.text, height),
+        toValue: getPosition({
+          text: text,
+          items: rotateItems,
+          height,
+        }),
         duration,
         useNativeDriver: true,
       }).start();
@@ -134,11 +196,12 @@ class TextRotator extends Component {
 
   render() {
     const { animation } = this.state;
-    const { textStyle, height } = this.props;
+    const { textStyle, height, rotateItems } = this.props;
+
     return (
       <View style={{ height }}>
         <Animated.View style={getAnimationStyle(animation)}>
-          {measureText.map(v => (
+          {rotateItems.map(v => (
             <Text key={v} style={textStyle}>
               {v}
             </Text>
@@ -149,4 +212,5 @@ class TextRotator extends Component {
   }
 }
 
+export { Tick, numberRange };
 export default RotateText;
